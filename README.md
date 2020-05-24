32ae981a10888b8b1d25f6a2ab7b22b60e32375a
Shogo Ogami <sg.tao.so@gmail.com>
2020-05-24 16:56:45 +0900

    add encode text to QR function

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 - refer : [Web QR](https://webqr.com/)
 - using API : http://goqr.me/api/doc
 - style reference : [webdesign-trens](https://webdesign-trends.net/entry/8148)
+- when edit image, use a site [PECO STEP](https://www.peko-step.com/tool/alphachannel.html)
 
---
+++ js/main.js
@@ -0,0 +1,41 @@
+{
+  'use strict';
+
+  const encode_qr_url = 'https://api.qrserver.com/v1/create-qr-code/?';
+  const charset = 'charset-source=UTF-8';
+  const size = '&size=150x150';
+
+  const textarea = document.querySelector('#textarea');
+  const encode_btn = document.querySelector('#encode_btn');
+  const msg = document.querySelector('#msg');
+  const qrCode = document.querySelector('#qrcode');
+  const qr_img = document.querySelector('#qrimg');
+  var first_load = false;
+
+  function encodeQR() {
+    // ex) https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example
+    let textdata = textarea.value;
+    console.log(`encodeQR : ${textdata}`);
+    conv_text = encodeURIComponent(textdata);
+    msg.textContent = '';
+    if (textdata == "") {
+      msg.textContent = "Failed!(textarea is blank)";
+      return;
+    } else {
+      msg.textContent = `convert a text: "${textdata}" `;
+    }
+
+    qr_img.setAttribute("src", encode_qr_url + charset + size + '&data=' + conv_text);
+    qr_img.setAttribute("id", "qrimg");
+    if (first_load === false) {
+      qrCode.appendChild(qr_img);
+      first_load = true;
+    }
+
+  }
+
+  encode_btn.addEventListener('click', encodeQR);
+  
+
+
+}
